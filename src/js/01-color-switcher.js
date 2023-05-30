8ea954513d0f2d213bca2323baeaefbe23d30cb5
Anton Hetman <00kyp1d0n41k00@gmail.com>
2023-05-30 18:54:10 +0300

    1+ 18:54 30.05

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let randomColor = getRandomHexColor();
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+stopBtn.disabled = true;
+startBtn.addEventListener('click', () => {
+  randomColor = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(randomColor);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
